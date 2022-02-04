29c230c8aa8311f0f41e74c6a70a50edb756acfc
RCMast3r <rcmast3r1@gmail.com>
2022-02-03 23:44:37 -0500

    asdf

---
+++ inclass1.m
@@ -0,0 +1,18 @@
+x=exp(i*pi)+1
+
+y=cos(pi)+(i*sin(pi))
+
+z=1;
+j = 2;
+while(j<100000)
+    
+    z = z + (1/(j^2))
+    j = j+1;
+end
+
+a = 10^(log10(8))
+
+b = log(exp(1))
+
+c = ((log10(pi)))/(log10((exp(1))))
+c2 = log(pi)
